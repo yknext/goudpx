6ea3f6d9d3dda73a126fa1e4c42117481e4534e8
yknext <yangkang2014@gmail.com>
2022-05-29 00:52:47 +0800

    test

--- cmd/goudpx/main.go
+++ cmd/goudpx/main.go
@@ -1,36 +1,35 @@
 package main
 
 import (
-	"github.com/firstrow/tcp_server"
 	"github.com/yknext/goudpx/pkg/service"
 )
 
 func main() {
 
-	server := tcp_server.New("0.0.0.0:6666")
-
-	server.OnNewClient(func(c *tcp_server.Client) {
-
-		udpChan := make(chan []byte, 4096)
-		go service.ReadUdpMulticastH264("239.93.0.184:5140", udpChan)
-		// new client connected
-		// lets send some message
-		for {
-			err := c.SendBytes(<-udpChan)
-			if err != nil {
-				return
-			}
-		}
-
-	})
-	server.OnNewMessage(func(c *tcp_server.Client, message string) {
-		// new message received
-	})
-	server.OnClientConnectionClosed(func(c *tcp_server.Client, err error) {
-		// connection with client lost
-	})
-
-	server.Listen()
+	//server := tcp_server.New("0.0.0.0:6666")
+	//
+	//server.OnNewClient(func(c *tcp_server.Client) {
+	//
+	//	udpChan := make(chan []byte, 4096)
+	//	go service.ReadUdpMulticastH264("239.93.0.184:5140", udpChan)
+	//	// new client connected
+	//	// lets send some message
+	//	for {
+	//		err := c.SendBytes(<-udpChan)
+	//		if err != nil {
+	//			return
+	//		}
+	//	}
+	//
+	//})
+	//server.OnNewMessage(func(c *tcp_server.Client, message string) {
+	//	// new message received
+	//})
+	//server.OnClientConnectionClosed(func(c *tcp_server.Client, err error) {
+	//	// connection with client lost
+	//})
+	//
+	//server.Listen()
 
 	srv := service.NewService()
 	srv.Run(":7777")
