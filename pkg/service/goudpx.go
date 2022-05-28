43745292e614e4afb8db2e90eb231f2785330aea
yknext <yangkang2014@gmail.com>
2022-05-28 14:25:17 +0800

    go func

--- pkg/service/goudpx.go
+++ pkg/service/goudpx.go
@@ -36,50 +36,6 @@ type Response struct {
 	Headers Header
 }
 
-// NewBroadcaster creates a new UDP multicast connection on which to broadcast
-func NewBroadcaster(address string) (*net.UDPConn, error) {
-	addr, err := net.ResolveUDPAddr("udp", address)
-	if err != nil {
-		return nil, err
-	}
-
-	conn, err := net.DialUDP("udp", nil, addr)
-	if err != nil {
-		return nil, err
-	}
-
-	return conn, nil
-}
-
-// Listen binds to the UDP address and port given and writes packets received
-// from that address to a buffer which is passed to a hander
-func Listen(address string, handler func(*net.UDPAddr, int, []byte)) {
-	// Parse the string address
-	addr, err := net.ResolveUDPAddr("udp", address)
-	if err != nil {
-		log.Println(err)
-	}
-
-	// Open up a connection
-	conn, err := net.ListenMulticastUDP("udp", nil, addr)
-	if err != nil {
-		log.Println(err)
-	}
-
-	conn.SetReadBuffer(maxDatagramSize)
-
-	// Loop forever reading from the socket
-	for {
-		buffer := make([]byte, maxDatagramSize)
-		numBytes, src, err := conn.ReadFromUDP(buffer)
-		if err != nil {
-			log.Println("ReadFromUDP failed:", err)
-		}
-
-		handler(src, numBytes, buffer)
-	}
-}
-
 func readUdpMulticasH264(UDP4MulticastAddress string, udpChan chan []byte) {
 
 	log.Println("Waiting for a RTP/H264 stream on UDP port 9000 - you can send one with GStreamer:\n" +
@@ -87,33 +43,47 @@ func readUdpMulticasH264(UDP4MulticastAddress string, udpChan chan []byte) {
 		" ! x264enc speed-preset=veryfast tune=zerolatency bitrate=600000" +
 		" ! rtph264pay ! udpsink host=127.0.0.1 port=9000")
 
+	mcaddr, err := net.ResolveUDPAddr("udp", UDP4MulticastAddress)
+	if err != nil {
+		fmt.Println("addr err:=", UDP4MulticastAddress)
+	}
+
+	socket, err := net.ListenMulticastUDP("udp4", nil, mcaddr)
+
 	// receive
 	data := make(chan []byte)
 
-	Listen(UDP4MulticastAddress, func(addr *net.UDPAddr, n int, buf []byte) {
-		log.Println(addr, n, string(buf[:n]))
-		data <- buf[:n]
-	})
-
-	var pkt rtp.Packet
-	for {
-		// parse RTP packet
-		err := pkt.Unmarshal(<-data)
-		if err != nil {
-			panic(err)
+	go func() {
+		for {
+			udpData := make([]byte, 4096)
+			n, _, err := socket.ReadFromUDP(udpData)
+			if err != nil {
+				fmt.Print("read udp stream err:=", err.Error())
+			} else {
+				data <- udpData[:n]
+			}
 		}
+	}()
 
-		// read from packet
-		byts := make([]byte, maxPacketSize)
-		n, err := pkt.MarshalTo(byts)
-		if err != nil {
-			panic(err)
+	go func() {
+		var pkt rtp.Packet
+		for {
+			// parse RTP packet
+			err := pkt.Unmarshal(<-data)
+			if err != nil {
+				fmt.Println("err:=", err.Error())
+			} else {
+				// read from packet
+				byts := make([]byte, maxPacketSize)
+				n, err := pkt.MarshalTo(byts)
+				if err != nil {
+					fmt.Println("err packet:", err.Error())
+				}
+				// send to http stream
+				udpChan <- byts[:n]
+			}
 		}
-		byts = byts[:n]
-
-		// send to http stream
-		udpChan <- byts
-	}
+	}()
 
 }
 
